9ab724584f04d2e1b58e6f4e34de4629dab0d513
jitendra-dev <jitendra.dhadavi.dev@gmail.com>
2025-01-02 16:37:19 +0530

    chore: project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AUTH_ROUTES } from './pages/auth/auth.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, //default route
+  ...AUTH_ROUTES,
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((c) => c.HomeComponent),
+  },
+];
